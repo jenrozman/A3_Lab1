2a0d847d46dfadfab03113002461153169cada80
Rozman <j_rozman@fanshawec.ca>
2017-11-24 11:42:14 -0500

    adding finished lab files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
 (function () {
-  // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
-   
+  // calling in the html
+    var theImages = document.querySelectorAll('.data-ref'),
+ 	      name = document.querySelector('.modelName'),
+ 		    price = document.querySelector('.priceInfo'),
+		    desc = document.querySelector('.modelDetails');
+ 	      
+function changeCar() {
+//console.log('halp');
+ let objectIndex = carData[this.id];
+          //changes the name/price/desc
+  	  name.firstChild.nodeValue = objectIndex.headline;
+      price.firstChild.nodeValue = objectIndex.text;
+      desc.firstChild.nodeValue = objectIndex.description;
+    
+          //lowers img opacity
+    theImages.forEach(function(element, index){
+        element.classList.add('nonActive');
+    })
+          //when selected opacity goes back to 100
+    this.classList.remove('nonActive');
+ }
+          //creates the listener to call in the changeCar function
+  theImages.forEach(function(element) {
+    element.addEventListener('click', changeCar, false);
+
+ 		 });
+
+
 
 })();
+
+
+
